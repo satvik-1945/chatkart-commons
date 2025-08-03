ee719cbf3c3e6161373be0f06bbf7e014c99d2fd
satvik-1945 <satviktejas@gmail.com>
2025-08-03 15:30:54 +0530

    MongoDBClient updated

--- commons/utils/MongoDBClient.py
+++ commons/utils/MongoDBClient.py
@@ -9,20 +9,20 @@ class MongoDBClient:
         _client = None
         _db = None
 
-        @classmethod
-        def init_connection(cls):
-            if cls._client is None:
-                try:
-                    cls._client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
-                    cls._client.admin.command('ping')
-                    cls._db = cls._client[DB_name]
-                    logger.info("✅ MongoDB connection established.")
-                except errors.ConnectionError as e:
-                    logger.error(f"❌ Failed to connect to MongoDB: {e}")
-                    raise
+    @classmethod
+    def init_connection(cls):
+        if cls._client is None:
+            try:
+                cls._client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
+                cls._client.admin.command('ping')
+                cls._db = cls._client[DB_name]
+                logger.info("✅ MongoDB connection established.")
+            except errors.ConnectionError as e:
+                logger.error(f"❌ Failed to connect to MongoDB: {e}")
+                raise
 
 
-        def get_collection(cls, name):
-            if cls._db is None:
-                cls.init_connection()
-            return cls._db[name]
+    def get_collection(cls, name):
+        if cls._db is None:
+            cls.init_connection()
+        return cls._db[name]
